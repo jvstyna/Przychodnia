f6886d399008a05e3ccb414361ca989ef58c0d26
jvstyna <j.krzywdziak@gmail.com>
2022-06-01 00:46:04 +0200

    added names for links

--- przychodnia_app/urls.py
+++ przychodnia_app/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dodaj_lekarza/', views.dodaj_lekarza),
-    path('dodaj_pacjenta/', views.dodaj_pacjenta),
-    path('dodaj_recepte/', views.dodaj_recepte),
-    path('dodaj_lek/', views.dodaj_lek),
-    path('wyswietl_lek/', views.wyswietl_lek),
-    path('wyswietl_recepte/', views.wyswietl_recepte),
-    path('wyswietl_pacjenta/', views.wyswietl_pacjenta),
-    path('wyswietl_lekarza/', views.wyswietl_lekarza),
+    path('dodaj_lekarza/', views.dodaj_lekarza, name='dodaj_lekarza'),
+    path('dodaj_pacjenta/', views.dodaj_pacjenta, name='dodaj_pacjenta'),
+    path('dodaj_recepte/', views.dodaj_recepte, name='dodaj_recepte'),
+    path('dodaj_lek/', views.dodaj_lek, name='dodaj_lek'),
+    path('wyswietl_lek/', views.wyswietl_lek, name='wyswietl_lek'),
+    path('wyswietl_recepte/', views.wyswietl_recepte, name='wyswietl_recepte'),
+    path('wyswietl_pacjenta/', views.wyswietl_pacjenta, name='wyswietl_pacjenta'),
+    path('wyswietl_lekarza/', views.wyswietl_lekarza, name='wyswietl_lekarza'),
 ]
