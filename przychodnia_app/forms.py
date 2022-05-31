6a8f0ea9647326e3510f14a10f2ca879666371cc
jvstyna <j.krzywdziak@gmail.com>
2022-06-01 00:46:29 +0200

    created widgets for all model forms in forms.py

--- przychodnia_app/forms.py
+++ przychodnia_app/forms.py
@@ -5,22 +5,53 @@ from .models import Lekarz, Pacjent, Lek, Recepta
 class DodajLekarza(forms.ModelForm):
     class Meta:
         model = Lekarz
-        fields = '__all__'
+        fields = ('imie_nazwisko', 'numer_telefonu', 'email', 'specjalizacja')
+
+        widgets = {
+            'imie_nazwisko': forms.TextInput(attrs={'class': 'form-control'}),
+            'numer_telefonu': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'specjalizacja': forms.Select(attrs={'class': 'form-control'})
+        }
 
 
 class DodajPacjenta(forms.ModelForm):
     class Meta:
         model = Pacjent
-        fields = '__all__'
+        fields = ('imie_nazwisko', 'numer_telefonu', 'email', 'plec', 'data_urodzenia', 'pesel')
+
+        widgets = {
+            'imie_nazwisko': forms.TextInput(attrs={'class': 'form-control'}),
+            'numer_telefonu': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'plec': forms.Select(attrs={'class': 'form-control'}),
+            'data_urodzenia': forms.SelectDateWidget(years=range(1900, 2022), attrs={'class': 'form-control'}),
+            'pesel': forms.NumberInput(attrs={'class': 'form-control'})
+        }
 
 
 class DodajLek(forms.ModelForm):
     class Meta:
         model = Lek
-        fields = '__all__'
+        fields = ('nazwa', 'substancja_czynna', 'cena')
+
+        widgets = {
+            'nazwa': forms.TextInput(attrs={'class': 'form-control'}),
+            'substancja_czynna': forms.TextInput(attrs={'class': 'form-control'}),
+            'cena': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 
 class DodajRecepte(forms.ModelForm):
     class Meta:
         model = Recepta
         fields = '__all__'
+        fields = ('pacjent', 'lekarz', 'lek', 'data', 'zalecenie')
+
+        widgets = {
+            'pacjent': forms.Select(attrs={'class': 'form-control'}),
+            'lekarz': forms.Select(attrs={'class': 'form-control'}),
+            'lek': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'data': forms.SplitDateTimeWidget(attrs={'class': 'form-control'}),
+            'zalecenia': forms.Textarea(attrs={'class': 'form-control'})
+        }
