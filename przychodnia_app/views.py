ea6990487d21449eb7fdf2236af835192860d9c2
jvstyna <j.krzywdziak@gmail.com>
2022-06-01 02:35:59 +0200

    added exporting views

--- przychodnia_app/views.py
+++ przychodnia_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import DodajLek, DodajLekarza, DodajRecepte, DodajPacjenta
 from .models import Pacjent, Lekarz, Lek, Recepta
+import csv
+import datetime
 
 
 def index(request):
@@ -90,3 +92,55 @@ def wyswietl_lekarza(request):
     return render(request, 'wyswietl_lekarza.html', {
         'lekarze': lekarze
     })
+
+
+def export_leki(request):
+    response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    writer.writerow(['Nazwa', 'Substancja czynna', 'Cena'])
+
+    for lek in Lek.objects.all().values_list('nazwa', 'substancja_czynna', 'cena'):
+        writer.writerow(lek)
+
+    response['Content-Disposition'] = f'attachment; filename="leki_{datetime.date.today()}.csv"'
+
+    return response
+
+
+def export_lekarze(request):
+    response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    writer.writerow(['Imie i Nazwisko', 'Numer telefonu', 'Email', 'Specjalizacja'])
+
+    for lekarz in Lekarz.objects.all().values_list('imie_nazwisko', 'numer_telefonu', 'email', 'specjalizacja'):
+        writer.writerow(lekarz)
+
+    response['Content-Disposition'] = f'attachment; filename="lekarze_{datetime.date.today()}.csv"'
+
+    return response
+
+
+def export_pacjenci(request):
+    response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    writer.writerow(['Imie i Nazwisko', 'Numer telefonu', 'Email', 'Plec', 'Data urodzenia', 'Pesel'])
+
+    for pacjent in Pacjent.objects.all().values_list('imie_nazwisko', 'numer_telefonu', 'email', 'plec', 'data_urodzenia', 'pesel'):
+        writer.writerow(pacjent)
+
+    response['Content-Disposition'] = f'attachment; filename="pacjenci_{datetime.date.today()}.csv"'
+
+    return response
+
+
+def export_recepty(request):
+    response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    writer.writerow(['Lek', 'Data', 'Pacjent', 'Lekarz', 'Zalecenie'])
+
+    for recepta in Recepta.objects.all().values_list('lek', 'data', 'pacjent', 'lekarz', 'zalecenie'):
+        writer.writerow(recepta)
+
+    response['Content-Disposition'] = f'attachment; filename="recepty_{datetime.date.today()}.csv"'
+
+    return response
